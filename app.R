95ba027d04b06a1cf68d862bb4d3608dd094aff7
Oluoch <K.Oluoch@cgiar.org>
2019-01-28 08:51:55 +0300

    Set default values
    The values set are arbitrary and will be changed to more convenient ones later.

--- app.R
+++ app.R
@@ -18,22 +18,22 @@ ui = dashboardPage(
       h2("INPUTS"),
       div( style = "background-color: rgb(120, 120, 120);color:black;height:120px",
            h4("Farm Information"),
-           numericInput(inputId = 'in1', label = "1. Farm Size (HA)", value = 0, min = 0, max = NA, step = NA)
+           numericInput(inputId = 'in1', label = "1. Farm Size (HA)", value = 10, min = 0, max = NA, step = NA)
            ),
       
       div( style = "background-color: rgb(180, 180, 180);color:black;height:300px",
            h4("Fertilizer Information"),
            selectInput(inputId = 'in2', label = '2. Fertilizer Mass Fraction', 
                   choices = c("NA", "Fert 1", "Fert 2", "Fert 3", "Fert 4") ),
-           numericInput(inputId = 'in3', label = "3. Fertilizer Quatity (KG)", value = 0, min = 0, max = NA, step = NA),
+           numericInput(inputId = 'in3', label = "3. Fertilizer Quatity (KG)", value = 300, min = 0, max = NA, step = NA),
       
-           numericInput(inputId = 'in4', label = "4. Cost ($)", value = 0, min = 0, max = NA, step = NA)
+           numericInput(inputId = 'in4', label = "4. Cost ($)", value = 100, min = 0, max = NA, step = NA)
       ),
       
       div( style = "background-color: rgb(240, 240, 240);color:black;height:200px",
            h4("Maize Information"),
-           numericInput(inputId = 'in5', label = "5. Attainable Yield (KG)", value = 0, min = 0, max = NA, step = NA),
-           numericInput(inputId = 'in6', label = "6. Price (per HA)", value = 0, min = 0, max = NA, step = NA)
+           numericInput(inputId = 'in5', label = "5. Attainable Yield (KG)", value = 20, min = 0, max = NA, step = NA),
+           numericInput(inputId = 'in6', label = "6. Price (per HA)", value = 30, min = 0, max = NA, step = NA)
       ),
       
       # Auto hide advanced options unless needed
