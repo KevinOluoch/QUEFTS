a05abcbe1f49d36cd9af73cd3401816e7cb6d1fd
Oluoch <K.Oluoch@cgiar.org>
2019-01-29 16:33:55 +0300

    Add all needed Inputs
    The next step will be to connect the inputs to the model before rearranging the inputs to better fit the screen.

--- app.R
+++ app.R
@@ -17,36 +17,44 @@ ui = dashboardPage(
       # The Inputs to the Model
       h2("INPUTS"),
       div( style = "background-color: rgb(120, 120, 120);color:black;height:120px",
-           h4("Farm Information"),
-           numericInput(inputId = 'in1', label = "1. Farm Size (HA)", value = 10, min = 0, max = NA, step = NA)
+           h4("1 Farm Information"),
+           numericInput(inputId = 'in11', label = "1.1 Farm Size (HA)", value = 10, min = 0, max = NA, step = NA)
            ),
       
       div( style = "background-color: rgb(180, 180, 180);color:black;height:300px",
-           h4("Fertilizer Information"),
-           selectInput(inputId = 'in2', label = '2. Fertilizer Mass Fraction', 
-                  choices = c("NA", "Fert 1", "Fert 2", "Fert 3", "Fert 4") ),
-           numericInput(inputId = 'in3', label = "3. Fertilizer Quatity (KG)", value = 300, min = 0, max = NA, step = NA),
-      
-           numericInput(inputId = 'in4', label = "4. Cost ($)", value = 100, min = 0, max = NA, step = NA)
+           h4("2 Site Soil Nutrients"),
+           numericInput(inputId = 'in21', label = "2.1 soilC (g/kg)", value = 300, min = 0, max = NA, step = NA),
+           numericInput(inputId = 'in22', label = "2.2 soilPolsen (p-OLSEN, mg/kg)", value = 300, min = 0, max = NA, step = NA),
+           numericInput(inputId = 'in23', label = "2.3 soilK (exchangeable K, mmol/kg)", value = 300, min = 0, max = NA, step = NA)
+      ),
+      div(
+           h4("3 Fertilizer Information"),
+           selectInput(inputId = 'in31', label = '3.1 Fertilizer Mass Fraction', 
+                  choices = c("NA", "Fert 1", "Fert 2", "Fert 3") ),
+           numericInput(inputId = 'in32', label = "3.2 Fertilizer 1 Quatity (KG)", value = 300, min = 0, max = NA, step = NA),
+           numericInput(inputId = 'in33', label = "3.3 Fertilizer 2 Quatity (KG)", value = 300, min = 0, max = NA, step = NA),
+           numericInput(inputId = 'in34', label = "3.4 Fertilizer 3 Quatity (KG)", value = 300, min = 0, max = NA, step = NA),
+           
+           numericInput(inputId = 'in35', label = "3.5 Cost ($)", value = 100, min = 0, max = NA, step = NA)
       ),
       
       div( style = "background-color: rgb(240, 240, 240);color:black;height:200px",
-           h4("Maize Information"),
-           numericInput(inputId = 'in5', label = "5. Attainable Yield (KG)", value = 20, min = 0, max = NA, step = NA),
-           numericInput(inputId = 'in6', label = "6. Price (per HA)", value = 30, min = 0, max = NA, step = NA)
+           h4("4 Maize Information"),
+           numericInput(inputId = 'in41', label = "4.1 Attainable Yield (KG)", value = 20, min = 0, max = NA, step = NA),
+           numericInput(inputId = 'in42', label = "4.2 Price (per HA)", value = 30, min = 0, max = NA, step = NA)
       ),
       
       # Auto hide advanced options unless needed
-      a(id = "AdvancedOptions", "Advanced Options"),
+      a(id = "AdvancedOptions", "5 Advanced Options"),
       shinyjs::hidden(
         div(id = "advanced",
-            numericInput(inputId = "in7", label = "Advanced Option 1", value = 30, min = 0, max = NA, step = NA),
-            numericInput(inputId = "in8", label = "Advanced Option 2", value = 45, min = 0, max = NA, step = NA)
+            numericInput(inputId = "in51", label = "5.1 Advanced Option 1", value = 30, min = 0, max = NA, step = NA),
+            numericInput(inputId = "in52", label = "5.2 Advanced Option 2", value = 45, min = 0, max = NA, step = NA)
         )
       ),
       actionButton(inputId = 'run', label = "RUN")
-      
-    )),
+      )
+    ),
     
     # SHOW input value in the meantime
   dashboardBody(
